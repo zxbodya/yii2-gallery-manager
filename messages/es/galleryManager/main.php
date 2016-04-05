9bc6ab7b441e9c654ed1e7b9d7dccb8bff8a73ed
nico <nicolas.p.suarez@gmail.com>
2016-04-05 09:01:48 -0300

    added spanish translation

---
+++ messages/es/galleryManager/main.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Spanish message translations.
+ */
+return [
+    'Add…' => 'Añadir…',
+    'Close' => 'Cerrar',
+    'Description' => 'Descripción',
+    'Edit information' => 'Editar información',
+    'Edit' => 'Editar',
+    'Name' => 'Nombre',
+    'Remove' => 'Eliminar',
+    'Save changes' => 'Guardar cambios',
+    'Select all' => 'Seleccionar todo',
+    'Drop Files Here…' => 'Arrastrar archivos aquí…',
+    'Uploading images…' => 'Subiendo imágenes…',
+];
