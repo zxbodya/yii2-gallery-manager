4d2e8672f48a5012b398fcde35010fcf49ad66f2
douglasmk <douglasmk@users.noreply.github.com>
2016-09-27 00:21:31 -0300

    Brazilian message translations.

---
+++ messages/pt-BR/galleryManager/main.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Brazilian message translations.
+ */
+return [
+    'Add…' => 'Adicionar…',
+    'Close' => 'Fechar',
+    'Description' => 'Descrição',
+    'Edit information' => 'Alterar informação',
+    'Edit' => 'Alterar',
+    'Name' => 'Nome',
+    'Remove' => 'Excluir',
+    'Save changes' => 'Savar alterações',
+    'Select all' => 'Selecionar todos',
+    'Drop Files Here…' => 'Solte os arquivos aqui…',
+    'Uploading images…' => 'Fazendo o upload das imagens…',
+];
